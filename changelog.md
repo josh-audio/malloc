fbf71ae23e490ab71a343021ff43de85fdecb177
Joshua Wade <6700184+josh-audio@users.noreply.github.com>
2025-02-22 00:38:10 -0500

    Allow memory cells to be edited in UI

--- changelog.md
+++ changelog.md
@@ -6,13 +6,13 @@
 
 - UI
   - Memory addresses are now displayed as small labels above cells, and cells now contain memory values.
-  - UI elements have been scaled to around 75% of their old sizes.
-  - The memory area itself can now vertically scroll, and the jank around resizing the command area has been fixed.
   - Added support for displaying memory addresses and values in hex. This can be changed with:
     ```
     setDisplayBase(10); // base 10
     setDisplayBase(16); // base 16
     ```
+  - Memory cells are now editable - click in the cell to edit.
+  - The visualization is now driven by the actual memory state. If the state is invalid, the visualization will reflect that.
 - Language runtime
   - Added support for hex literals, e.g.:
     ```c
@@ -38,7 +38,15 @@
     ```
 
     Besides addressing some minor bugs, being more careful with types paves the way for better handling of new pointer types - see below for more.
-  - Added support for pointer types, and for reading to and writing from memory.
+  - Added support for pointer types, and for reading to and writing from memory. The end goal is to support at least some of the examples from OSTEP chapter 14, for example:
+    ```c
+    int *x = (int *) malloc(sizeof(int));
+    ```
+    ```c
+    char *src = "hello";
+    char *dst = (char *) malloc(strlen(src) + 1);
+    strcpy(dst, src);
+    ```
 
     The memory cells in the UI will update when they are written to.
 
@@ -65,5 +73,9 @@
 
 ### Maintenance:
 
-- Ported to TypeScript / React 19 / Vite, with Zod for AST type safety
-- Refactored UI to use app-level state store with MobX
+- Tooling
+  - Ported to TypeScript / React 19 / Vite, with Zod for AST type safety
+  - Refactored UI to use app-level state store with MobX
+- UI
+  - UI elements have been scaled to around 75% of their old sizes.
+  - The memory area itself can now vertically scroll, and the jank around resizing the command area has been fixed.
--- src/components/MemoryCell.tsx
+++ src/components/MemoryCell.tsx
@@ -1,10 +1,21 @@
 import { observer } from "mobx-react";
 import state from "../state/state";
+import { useEffect, useRef, useState } from "react";
 
 const MemoryCell = observer(
   (props: {
-    cellState: { index: number; isAllocated: boolean; isReserved: boolean; error: boolean; };
+    cellState: {
+      index: number;
+      isAllocated: boolean;
+      isReserved: boolean;
+      error: boolean;
+    };
   }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [edit, setEdit] = useState(false);
+    const [inputValue, setInputValue] = useState("");
+
     const base = state.displayBase;
 
     let address = props.cellState.index.toString(base).toUpperCase();
@@ -17,19 +28,60 @@ const MemoryCell = observer(
       value = value.padStart(2, "0");
     }
 
+    // Focus the input when it is shown
+    useEffect(() => {
+      if (inputRef.current?.style.display !== "none") {
+        inputRef.current?.focus();
+      }
+    });
+
     return (
       <div
         className={
           "cell-container" +
           (props.cellState.isAllocated ? " allocated" : " unallocated") +
-          (props.cellState.isReserved ? " reserved" : " unreserved") + 
+          (props.cellState.isReserved ? " reserved" : " unreserved") +
           (props.cellState.error ? " error" : "")
         }
+        onClick={() => {
+          setEdit(true);
+          setInputValue(value);
+        }}
       >
-        <div className="memory-value">{value}</div>
+        <div className="memory-value">{edit ? inputValue : value}</div>
         <div className={"memory-address-parent"}>
           <div className={"memory-address"}>{address}</div>
         </div>
+        <input
+          ref={inputRef}
+          type="text"
+          value={inputValue}
+          style={{
+            display: edit ? undefined : "none",
+          }}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+
+            const newValue = Math.max(
+              Math.min(parseInt(e.target.value, base), 255),
+              0
+            );
+
+            if (isNaN(newValue)) {
+              return;
+            }
+
+            state.heap[props.cellState.index] = newValue;
+          }}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              setEdit(false);
+            }
+          }}
+          onBlur={() => {
+            setEdit(false);
+          }}
+        />
       </div>
     );
   }
